b04f7e7efffa4e34f1917bfe8fb209bd1521b099
Yanthe Penne <yanthe.penne@gmail.com>
2023-08-30 02:27:38 +0200

    Extended Euclid

---
+++ Fundamenten Wiskunde/Extended Euclidian.py
@@ -0,0 +1,19 @@
+import Modulus as m
+# Extended Euclidian Algorithm
+
+"""
+Beschrijving: 
+    Toegevoegde Extended Euclidian Algorithm. 
+
+Input:
+    gcd(number1, number2)
+
+Step:
+    gcd(number2, mod(number1, number2))
+    
+Output: 
+    20 = 4 * 5
+    15 = 3 * 5
+    gcd(20,15) = 5
+    
+"""
