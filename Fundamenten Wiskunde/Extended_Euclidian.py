9cf60f54fea5ac80efb911343f76e8e0cc311955
Yanthe Penne <yanthe.penne@gmail.com>
2023-08-30 03:21:13 +0200

    extend Euclid

--- Fundamenten Wiskunde/Extended_Euclidian.py
+++ Fundamenten Wiskunde/Extended_Euclidian.py
@@ -18,8 +18,24 @@ Output:
     
 """
 def extended_gcd(a, b): 
-    d = ea.gcd(a, b)
-    alfa = 
-    beta = 
+    # d = ea.gcd(a, b)
+    r0 = a
+    a0 = 1
+    b0 = 0
 
-    print(f"{d} = {alfa} * {a} + {beta} * {b}")
+    r1 = b
+    a1 = 0 
+    b1 = 1
+  
+    
+    while r1 != 0: 
+        q = r0/r1
+        r0 = r1
+        r1 = r0 - q*r1
+        a1 = a0 - q*a1 
+        b1 = b0 - q*b1
+
+    return print(f"{r1} = {a1} * {a} + {b1} * {b}")
+
+
+print(extended_gcd(int(input("Number1: ")), int(input("Number2: "))))
