4f12b73f9b89dfbceb4bd89141d4ad7d04fd0272
Yanthe Penne <yanthe.penne@gmail.com>
2023-08-31 06:40:43 +0200

    congruent

---
+++ Codering/Lineaire_Blockcodes.py
@@ -0,0 +1,2 @@
+def Lineaire_Blockcodes(N, n, d, base):
+    print("Not yet implemented")
--- Fundamenten Wiskunde/Congruent.py
+++ Fundamenten Wiskunde/Congruent.py
@@ -1,5 +1,7 @@
 import math
-# Congruence Solver
+
+
+# Congruence Solver (Brute Force)
 """
 Beschrijving:
 
@@ -19,9 +21,13 @@ Output:
 
 def congruentie(a, e, m):
     # ax = e mod m
+    # Coprime
     checker = math.gcd(a, m) == 1
+
     if checker:
         for i in range(0, m):
             solution = (a * i) % m == e % m
             if solution:
                 return i
+
+    return False
