8c6e2d3bbc5457e7085fdc49e727a50e1cb36708
Yanthe Penne <yanthe.penne@gmail.com>
2023-08-31 22:20:44 +0200

    Lineaire blocks

--- Codering/Lineaire_Blockcodes.py
+++ Codering/Lineaire_Blockcodes.py
@@ -1,2 +1,55 @@
+import math
+# Decodeer een Lineaire Block Code
+"""
+Beschrijving: 
+
+
+Input: 
+
+
+
+
+Output: 
+
+
+
+
+"""
+
+
 def Lineaire_Blockcodes(N, n, d, base):
     print("Not yet implemented")
+
+
+def correction(d):
+    return (d - 1)/2
+
+
+"""
+def Lineaire_Blockcodes(f, d, base):
+
+def correction(d): 
+    return (d - 1)/2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
